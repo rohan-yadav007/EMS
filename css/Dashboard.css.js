d48c3cc46de5db363965004d50a6474059556ca5
bipul@mindztechnology.com <bipul@mindztechnology.com>
2020-04-20 17:01:51 +0530

    updated code

---
+++ css/Dashboard.css.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Module = styled.View`
+  flex-direction: column;
+`;
+export const Module2 = styled.Text`
+color:#fff;
+font-size:18px;
+`;
+
+export const CustomText = styled.Text`
+  font-size: ${props => props.fs || '40px'};
+  /* justify-content:'center'; */
+  text-align: center;
+  font-weight: bold;
+  color:#fff;
+`;
