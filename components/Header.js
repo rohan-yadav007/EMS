f379d42480d323b851b299ee491b29e21ec771ea
bipul@mindztechnology.com <bipul@mindztechnology.com>
2020-04-28 13:45:30 +0530

    updated code

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import Icon from 'react-native-vector-icons/Foundation';
-import Icon1 from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/AntDesign';
+import Icon1 from 'react-native-vector-icons/AntDesign';
 import {useNavigation} from '@react-navigation/native';
 import {connect} from 'react-redux';
 import {handleLogout} from '../redux/Action/login.action';
 
-const Header = (props) => {
+const Header = props => {
   const navigation = useNavigation();
- const title = props.title;
+  const title = props.title;
   return (
     <>
       <View style={styles.headerWrapper}>
         <TouchableOpacity
           onPress={() => navigation.openDrawer()}
           style={styles.hamburger}>
-          <Icon name="indent-more" size={35} color="#fff" />
+          <Icon name="menufold" size={30} color="#fff" />
         </TouchableOpacity>
 
-        
         <View style={styles.logoutButton}>
-          {title ? <Text style={styles.logoutText}>{title}</Text>:null}
+          {title ? <Text style={styles.logoutText}>{title}</Text> : null}
         </View>
-        
 
         <TouchableOpacity onPress={() => props.handleLogout()}>
-        <Icon1 name="logout" size={30} style={{marginTop:12}} color="#fff" />
+          <Icon1 name="login" size={25} style={{marginTop: 12}} color="#fff" />
         </TouchableOpacity>
       </View>
     </>
@@ -42,26 +40,26 @@ export default connect(
 const styles = StyleSheet.create({
   headerWrapper: {
     borderBottomWidth: 1,
-    backgroundColor: '#3875c3',
+    backgroundColor: '#077cff',
     borderBottomColor: '#53a8bf',
     justifyContent: 'space-between',
     flexDirection: 'row',
-    height:55
+    paddingLeft: 10,
+    paddingRight:10,
     // alignContent:'center'
   },
   hamburger: {
-    position:'absolute',
+    position: 'absolute',
     left: 10,
     marginTop: 10,
   },
   logoutText: {
-    lineHeight: 30,
+    lineHeight: 27,
     color: '#fff',
     fontSize: 22,
-   
   },
   logoutButton: {
     padding: 12,
-    marginLeft:'33%'
+    marginLeft: '33%',
   },
 });
